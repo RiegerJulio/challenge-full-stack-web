83ff30b669891a7a9f6c18f2ee6b2b0814e57e64
riegerjulio <riegerproducoes@gmail.com>
2022-08-26 23:08:44 -0300

    test: testing createStudent useCase

--- back-end/src/api/modules/students/useCases/Student/Create/CreateStudentUseCase.test.ts
+++ back-end/src/api/modules/students/useCases/Student/Create/CreateStudentUseCase.test.ts
@@ -1,6 +1,33 @@
+import StudentRepositoryInMemory from '../../../repositories/in-memory/StudentRepositoryInMemory';
+import CreateStudentUseCase from './CreateStudentUseCase';
+
+let createStudentUseCase: CreateStudentUseCase;
+let sudentRepositoryInMemory: StudentRepositoryInMemory;
+
+const student = {
+  name: "John Doe",
+  email: "john@doe.com",
+  cpf: "12345678910",
+  ra: "123456",
+}
+
 describe("Create Student", () => {
+  beforeEach(() => {
+    sudentRepositoryInMemory = new StudentRepositoryInMemory();
+    createStudentUseCase = new CreateStudentUseCase(sudentRepositoryInMemory);
+  });
   it("should be able to create a new student", async () => {
-    const sum = 2 + 2;
-    expect(sum).toBe(4);
+
+    await createStudentUseCase.execute(student);
+
+    const studentCreated = await sudentRepositoryInMemory.findByRa(student.ra);
+
+    expect(studentCreated).toHaveProperty("ra");
+  });
+
+  it("should not be able to create a new student with same ra", async () => {
+    await createStudentUseCase.execute(student);
+
+    await expect(createStudentUseCase.execute(student)).rejects.toEqual(new Error("Student already exists"));
   });
 });
